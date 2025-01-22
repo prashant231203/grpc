c28c90a3693e811619f6ed265bec9abf0e297c59
Prashant singh <135175230+prashant231203@users.noreply.github.com>
2025-01-22 13:51:08 +0530

    new

--- Server/main.go
+++ Server/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/prashant231203/grpc/proto" // Adjust the import path accordingly
+	"google.golang.org/grpc"
+)
+
+const PORT = ":8080" // Define your port here
+
+type helloServer struct {
+	pb.UnimplementedGreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", PORT)
+	if err != nil {
+		log.Fatalf("could not start the server: %v", err) // Corrected formatting
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("server started at: %v", lis.Addr()) // Corrected formatting
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to start the server: %v", err) // Corrected formatting
+	}
+}
--- client/main.go
+++ client/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	PORT = ":8080"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost"+PORT, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("could not connect to the server: %v", err)
+	}
+
+	defer conn.Close()
+
+	//
+
+}
